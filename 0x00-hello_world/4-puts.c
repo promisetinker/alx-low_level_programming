c1065fb2308557f3b60522c9354a32d8cebefaed
promisetinker <promisenwanesi4@gmail.com>
2023-02-16 17:11:55 -0800

    fifth C commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - will represent as the main function
+ *
+ * Return:  represent 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
+
