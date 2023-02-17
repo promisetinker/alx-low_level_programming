bc4d13f1ba71fe53cd8821b7c5a037dae199553e
promisetinker <promisenwanesi4@gmail.com>
2023-02-17 03:11:25 -0800

    sixth C commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - will represent as the main function
+ *
+ * Return: represents 0
+ */
+
+int main(void)
+{
+   printf("size of a char: %lu byte(s)",sizeof(char));
+   printf("size of int: %lu bytes(s)", sizeof(int));           
+   printf("size of a long int: %lu bytes(s)", sizeof(long int));
+   printf("size of a long long int: %lu bytes(s)", sizeof(long long int));
+   printf("size of a float: %lu bytes(s)", sizeof(float));
+   return (0);
+}
+
