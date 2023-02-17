d608c0ee20999320a0fb3b4a220efed3fb171fa4
promisetinker <promisenwanesi4@gmail.com>
2023-02-17 01:40:32 -0800

    fourth commit task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,13 @@
 /**
  * main - will represent as the main function
  *
- * Return:  represent 0
+ * Return: : represent 0
  */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
 
