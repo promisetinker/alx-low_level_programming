a838b66e4389481c0a67f66a5ee37950a09ae467
promisetinker <promisenwanesi4@gmail.com>
2023-02-17 01:55:30 -0800

    fifth C commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - will represent as the main function
+ *
+ * Return: represents 0
+ */
+
+int main(void)
+
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+
+}
