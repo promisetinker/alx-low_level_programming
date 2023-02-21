e18f3ebca5ea9555fed4d2d7f261ef01d6975c88
promisetinker <promisenwanesi4@gmail.com>
2023-02-21 15:11:47 -0800

    second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print function to print alphabet in lower case
+ *
+ * Return: represents 0;
+ */
+
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
