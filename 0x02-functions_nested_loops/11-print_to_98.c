70ad36acf349ee2fe0e4ce701b394225d8c01595
promisetinker <promisenwanesi4@gmail.com>
2023-02-22 06:53:00 -0800

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * add - adds two main nunbers
+ * @a: represents a
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n ; 1  <= 98 ; i++)
+		{
+			print("%d ", i);
+		}
+		print("\n");
+	}
+	else if (n < 0)
+	{
+	
+		for ("i = n" ; 1 <= 98; i++);
+		{
+			printf("%d ", i);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+
+		for (i - n);
+	}
+	else if (n == 98)
+		printf("%d ", n);
+}
