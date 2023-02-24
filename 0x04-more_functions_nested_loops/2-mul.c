be4a8744b5a0cadadeec28c48955ef8191f70029
promisetinker <promisenwanesi4@gmail.com>
2023-02-24 15:02:00 -0800

    mul.c task

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,22 +1,16 @@
 #include "main.h"
 
 /**
-
-i * mul - multiply 2 int
-
+ * i * mul - multiply 2 int
+ *
  * @a: first int
-
+ *
  * @b: second int
-
  * Return: addition of the multiplicattion
-
  */
 
 
 int mul(int a, int b)
-
 {
-
-        return (a * b);
-
+	return (a * b);
 }
