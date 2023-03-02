a906b20b3156fa3c8bb1e7397d14732a57bda8e4
promisetinker <promisenwanesi4@gmail.com>
2023-03-01 16:20:09 -0800

    more pointers task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - this reverses array of integers
+ * @a: array
+ * @n: number of arrayy
+ *
+ * Return: void
+ */
+
+
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	int j;
+		for (i = 0; i < n--; i++)
+		{
+			j = a[i];
+			a[i] = a[n];
+			a[n] = j;
+		}
+}
