a5876d1915e401fac34e089a5031d308296d11d6
promisetinker <promisenwanesi4@gmail.com>
2023-03-06 15:13:15 -0800

    final taak pointer to pointer

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: represents 0
+ */
+
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (k = 0; k < 8; k++)
+			_putchar(a[i][k]);
+		_putchar('\n');
+	}
+}
