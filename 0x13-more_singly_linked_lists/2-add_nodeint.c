b1aef4d3acdf2357b62b08a0001c2601d86fa330
promisetinker <promisenwanesi4@gmail.com>
2023-04-04 12:00:37 -0700

    2. Add node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,33 +1,29 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
-/*
- * Define: the structure for a linked list nod
- */
-listint_t *add_nodeint(listint_t **head, const int n);
-{
-	int n;
-	struct listint_s *next;
-}
-listint_t;
-
 /**
- * add_nodeint_end - Adds a new node at the end of a listint_t list
- * @head: A pointer to the head of the list
- * @n: The integer value to be stored in the new node
+ * add_nodeint - Adds a new node at the beginning of a listint_t list
+ * @head: Pointer to a pointer to the first node of the list
+ * @n: Value of the integer to store in the new node
  *
- * Return: The address of the new element, or NULL if it failed
+ * Return: Address of the new node or NULL if it failed
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node, *temp;
-	/* Create a new node and check for memory allocation errors */
-	new_node = malloc(sizeof(listint_t));
+	/* Create a new node with malloc */
+	listint_t *new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
-	{
+
 		return (NULL);
-	}
+
+	/* Set the value of the new node */
+	new_node->n = n;
+
+	/* Make the new node point to the old head of the list */
+	new_node->next = *head;
+
+	/* Make the head of the list point to the new node */
+	*head = new_node;
+
+	return (new_node);
 }
